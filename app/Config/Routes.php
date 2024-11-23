88780b4086b20b31db9792433f83d2f5e8b8028f
putributarbutar39 <butarbutarputri39@gmail.com>
2024-11-23 09:33:27 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,17 +7,16 @@ use CodeIgniter\Router\RouteCollection;
  */
 
 $routes->setAutoRoute(true);
-$routes->get('/', 'suara24\Login::login');
+$routes->get('/', 'Suara24\Login::login');
 $routes->get('hasil/data', 'Hasil::getHasilSuara');
 $routes->get('hasil/getget', 'Hasil::getSuara');
 
 $routes->add('suara24/logout', 'Suara24\Login::logout');
 
 $routes->group('suara24', ['filter' => 'noadmin'], function ($routes) {
-$routes->add('', '\Login::login');
+    $routes->add('', '\Login::login');
     $routes->add('login', 'Suara24\Login::login');
     $routes->add('lupapassword', 'Suara24\Login::lupapassword');
     $routes->add('resetpassword', 'Suara24\Login::resetpassword');
     $routes->get('send', 'Realtime::index');
-
 });
